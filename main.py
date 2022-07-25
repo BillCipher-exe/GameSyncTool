200c4667c256d9101ef5bfb97fda7dad21566a08
Hakan <hakan.baser@uni-wuppertal.de>
2022-07-25 16:59:48 +0200

    use config file for paths

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import support
 import db
+import configparser
+
+config = configparser.ConfigParser()
+config.read("config.ini")
+
+print(config["path"]["retroarch_saves"])
+
+def sync_retroarch(path):
+    local_update_list = support.get_newer_on_server(path)
+    for x in local_update_list:
+        db.get_file_db(path, x["id"])
+    server_update_list = support.get_newer_on_local(path)
+    for x in server_update_list:
+        db.put_file_db(path, x["filename"], x["mtime"])
 
 
-local_update_list = support.get_newer_on_server(
-    r"C:\Users\hakan\Desktop\Programming\synctool_python\test")
-for x in local_update_list:
-    db.get_file_db(
-        r"C:\Users\hakan\Desktop\Programming\synctool_python\test", x["id"])
-
-
-server_update_list = support.get_newer_on_local(
-    r"C:\Users\hakan\Desktop\Programming\synctool_python\test")
-for x in server_update_list:
-    db.put_file_db(
-        r"C:\Users\hakan\Desktop\Programming\synctool_python\test", x["filename"], x["mtime"])
-
+sync_retroarch(config["path"]["retroarch_saves"])
 
 #input("Press enter to exit ;)")
