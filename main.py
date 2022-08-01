e5b42203524a0099bc928949954b62eceb23a59f
Hakan <hakan.baser@uni-wuppertal.de>
2022-08-01 16:08:20 +0200

    code Database cleanup

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ config = configparser.ConfigParser()
 config.read("config.ini")
 
 if config["path"]["retroarch_saves"] != "none":
-    retroarch = db.Retroarch(config["database"],config["path"]["retroarch_saves"], "retroarch")
+    retroarch = db.Retroarch(
+        config["database"], config["path"]["retroarch_saves"], "retroarch")
     retroarch.sync()
 
 if config["path"]["dolphin_gc_saves"] != "none":
-    dolphin_GC = db.Dolphin_GC(config["database"],config["path"]["dolphin_gc_saves"], "dolphin_GC")
+    dolphin_GC = db.Dolphin_GC(
+        config["database"], config["path"]["dolphin_gc_saves"], "dolphin_GC")
     dolphin_GC.sync()
--- setup.py
+++ setup.py
@@ -5,25 +5,29 @@ import configparser
 from pip import main
 from os import path
 
+
 def install(package):
     try:
-        subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+        subprocess.check_call(
+            [sys.executable, "-m", "pip", "install", package])
     except:
         print("ERROR: Could not install Python Package. Check if pip3 is installed")
         sys.exit(1)
+
+
 install("mysql-connector-python")
 
 config = configparser.ConfigParser()
 config.read("config.ini")
 
 print("------------------------------------SETUP---------------------------------------")
-print("Input your MySQL Server host: ",end=" ")
+print("Input your MySQL Server host: ", end=" ")
 config["database"]["host"] = input()
-print("Input your MySQL Server user name: ",end=" ")
+print("Input your MySQL Server user name: ", end=" ")
 config["database"]["user"] = input()
-print("Input your MySQL Server password: ",end=" ")
+print("Input your MySQL Server password: ", end=" ")
 config["database"]["password"] = input()
-print("Input your MySQL Server database name: ",end=" ")
+print("Input your MySQL Server database name: ", end=" ")
 config["database"]["db"] = input()
 
 
@@ -36,10 +40,8 @@ try:
     )
     try:
         mycursor = mydb.cursor(dictionary=True, buffered=True)
-        add_retroarch = "CREATE TABLE IF NOT EXISTS retroarch (id INT NOT NULL AUTO_INCREMENT,filename VARCHAR(255), mtime DATETIME, file LONGBLOB, subfolder VARCHAR(255), PRIMARY KEY (id) )"
-        add_dolphin = "CREATE TABLE IF NOT EXISTS dolphin_GC (id INT NOT NULL AUTO_INCREMENT,filename VARCHAR(255), mtime DATETIME, file LONGBLOB, subfolder VARCHAR(255), PRIMARY KEY (id) )"
-        mycursor.execute(add_retroarch)
-        mycursor.execute(add_dolphin)
+        sql = "CREATE TABLE IF NOT EXISTS savegame (id INT NOT NULL AUTO_INCREMENT,filename VARCHAR(255), mtime DATETIME, file LONGBLOB, subfolder VARCHAR(255), emulator VARCHAR(255), PRIMARY KEY (id) )"
+        mycursor.execute(sql)
         mydb.commit()
         print("-------------------------------MySQL DB connected-------------------------------")
         print("--------------------------------------------------------------------------------")
@@ -53,10 +55,10 @@ except:
 
 print("-----------------------------Add Savegame Locations-----------------------------")
 
-print("Sync Retroarch? (Y/N): ",end=" ")
+print("Sync Retroarch? (Y/N): ", end=" ")
 choice = input()
 if choice == "y" or "Y":
-    print("Enter full path to Savegame folder (.../Retroarch/saves):",end=" ")
+    print("Enter full path to Savegame folder (.../Retroarch/saves):", end=" ")
     retroarch_path = input()
     if path.basename(retroarch_path) == "saves":
         config["path"]["retroarch_saves"] = retroarch_path
@@ -66,10 +68,10 @@ if choice == "y" or "Y":
 else:
     config["path"]["retroarch_saves"] = "none"
 
-print("Sync Dolphin (GameCube)? (Y/N): ",end=" ")
+print("Sync Dolphin (GameCube)? (Y/N): ", end=" ")
 choice = input()
 if choice == "y" or "Y":
-    print("Enter full path to Savegame folder (.../Dolphin Emulator/GC):",end=" ")
+    print("Enter full path to Savegame folder (.../Dolphin Emulator/GC):", end=" ")
     dolpin_gc_path = input()
     if path.basename(dolpin_gc_path) == "GC":
         config["path"]["dolphin_GC_saves"] = dolpin_gc_path
@@ -81,7 +83,7 @@ else:
 
 
 try:
-    config_file = open("config.ini","w")
+    config_file = open("config.ini", "w")
     config.write(config_file)
     config_file.close()
 except:
